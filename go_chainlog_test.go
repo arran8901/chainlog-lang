d4a1d6ebe3003957682bb0696ac4b8155d240b57
Arran O'S <arran.os.signup@gmail.com>
2022-06-03 06:57:48 +0100

    Add test for parametric disaster insurance full example

--- go_chainlog_test.go
+++ go_chainlog_test.go
@@ -21,6 +21,132 @@ func TestMsg(t *testing.T) {
 	runTestsForDir(t, "tests/msg")
 }
 
+func TestLib(t *testing.T) {
+	runTestsForDir(t, "tests/lib")
+}
+
+func TestParametricDisasterInsurance(t *testing.T) {
+	fileBytes, _ := ioutil.ReadFile("tests/full_examples/parametric_disaster_insurance.chl")
+	fileSource := string(fileBytes)
+
+	i := newTestInterpreter()
+	if err := i.Consult(fileSource); err != nil {
+		panic(err)
+	}
+	// 1656028800 is Jun 24 00:00 GMT
+
+	if err := i.prologInterpreter.Exec(`
+rainfall(13, 1654819200).  % Jun 10
+rainfall(27, 1654905600).  % Jun 11
+rainfall(81, 1654992000).  % Jun 12
+rainfall(81, 1655078400).  % Jun 13
+rainfall(81, 1655164800).  % Jun 14
+rainfall(81, 1655251200).  % Jun 15
+rainfall(81, 1655337600).  % Jun 16
+rainfall(81, 1655424000).  % Jun 17
+rainfall(81, 1655510400).  % Jun 18
+rainfall(81, 1655596800).  % Jun 19
+rainfall(81, 1655683200).  % Jun 20
+rainfall(81, 1655769600).  % Jun 21
+rainfall(56, 1655856000).  % Jun 22
+rainfall(46, 1655942400).  % Jun 23
+rainfall(23, 1656028800).  % Jun 24
+
+seismic_intensity('5+', 1672534801).  % After expiration
+
+wind_speed(99, 1655164800).  % Jun 14
+	`); err != nil {
+		panic(err)
+	}
+
+	// Expect cyclone 80%, flood 100%.
+	expectQueryDerivations(t, i, `eligible_for(D, P)`,
+		&Derivation{Successful: true, Unifications: map[string]string{"D": "cyclone", "P": "80"}},
+		&Derivation{Successful: true, Unifications: map[string]string{"D": "flood", "P": "100"}},
+		&Derivation{Successful: false},
+	)
+
+	// Attempt payout from wrong address.
+	_, err := i.Message(`claimPayout(cyclone)`, &MessageContext{
+		Sender:  "0x5555",
+		Value:   0,
+		Time:    1656288000,
+		Balance: 1000,
+	})
+	if err.Error() != "error(require_error, context(claimPayout(cyclone), msg_ctx('0x5555', 0, 1656288000, 1000), , (sender('0x5555'), policyholder('0x5555')), policyholder('0x5555')))" {
+		t.Fatalf("Expected require_error but got %s", err.Error())
+	}
+
+	// Make a cyclone payout (from correct address).
+	actions, err := i.Message(`claimPayout(cyclone)`, &MessageContext{
+		Sender:  "0x1234",
+		Value:   0,
+		Time:    1656288000, // Jun 27
+		Balance: 1000,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !(len(actions) == 2 &&
+		actions[0].Kind() == "assert" &&
+		actions[0].(AssertAction).Term == "claimed(cyclone,1656288000)" &&
+		actions[1].Kind() == "transfer" &&
+		actions[1].(TransferAction).ToAddress == "0x1234" &&
+		actions[1].(TransferAction).Value == 80) {
+		t.Fatalf("Expected actions [assert(claimed(cyclone,1656288000)), transfer(0x1234,80)], got %s", actions)
+	}
+	i.Assert(actions[0].(AssertAction).Term)
+
+	// Expect flood 100% but no longer cyclone
+	expectQueryDerivations(t, i, `eligible_for(D, P)`,
+		&Derivation{Successful: true, Unifications: map[string]string{"D": "flood", "P": "100"}},
+		&Derivation{Successful: false},
+	)
+
+	// Attempt cyclone payout when not eligible
+	_, err = i.Message(`claimPayout(cyclone)`, &MessageContext{
+		Sender:  "0x1234",
+		Value:   0,
+		Time:    1656374400, // Jun 28
+		Balance: 920,
+	})
+	if !(strings.Contains(err.Error(), "require_error") &&
+		strings.Contains(err.Error(), ", eligible_for(cyclone")) {
+		t.Fatalf("Expected require_error with eligible_for(cyclone, _) but got %s", err.Error())
+	}
+
+	// Dynamic KB should show cyclone has been claimed
+	dynamicKB := i.GetDynamicKB()
+	if strings.HasSuffix(dynamicKB, "claimed(cyclone, 1656288000).\n") {
+		t.Fatalf("expected dynamic KB of claimed(cyclone, 1656288000) at the end, got %s", dynamicKB)
+	}
+}
+
+func expectQueryDerivations(t *testing.T, i *Interpreter, query string, expectedDerivations ...*Derivation) {
+	itr, err := i.Query(query)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer itr.Close()
+
+	for i, expectedDerivation := range expectedDerivations {
+		actualDerivation, err := itr.Next()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		equal := expectedDerivation.Successful == actualDerivation.Successful &&
+			len(expectedDerivation.Unifications) == len(actualDerivation.Unifications)
+		for variable, unifiedTerm := range expectedDerivation.Unifications {
+			equal = equal && actualDerivation.Unifications[variable] == unifiedTerm
+		}
+
+		if !equal {
+			t.Errorf("For query %s, derivation %d: expected %+v, actual %+v", query, i, expectedDerivation, actualDerivation)
+		}
+	}
+}
+
 func runTestsForDir(t *testing.T, dir string) {
 	// Walk directory, collecting files with extension '.chl' as test files.
 	var testFilepaths []string
